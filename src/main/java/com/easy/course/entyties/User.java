a603cc74b39951782ad2e63e286c70a52aa53379
Easy-System-EF <easy_system@outlook.com>
2022-11-08 12:16:24 -0300

    Bco de dados H2

---
+++ src/main/java/com/easy/course/entyties/User.java
@@ -0,0 +1,107 @@
+package com.easy.course.entyties;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+/*
+ * renomeando tabela para evitar conflito (nome protegido do banco de dados
+ */
+@Table(name = "tb_user")
+public class User implements Serializable  {
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * indicador da chave do objeto
+	 */
+	@Id
+	/*
+	 * para incrementar chave
+	 */
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String phone;
+	private String email;
+	private String password;
+	
+	public User() {	
+	}
+
+	public User(Long id, String name, String phone, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
