02763fd76ea4e60f88f0300dc5d8f9d1f2aa189d
Easy-System-EF <easy_system@outlook.com>
2022-11-23 19:27:52 -0300

    Exception handling - update

--- src/main/java/com/easy/course/services/UserService.java
+++ src/main/java/com/easy/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.easy.course.repositories.UserRepository;
 import com.easy.course.services.exception.DataBaseException;
 import com.easy.course.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 /*
  * informando que serviço é um registro componente do spring no mecanismo de gestão de dependencia 
  */
@@ -62,9 +64,13 @@ public class UserService {
 	 * referenceById -> traz o obj monitorado sem trazer para atualizações e dps salva
 	 */
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity, User obj) {
